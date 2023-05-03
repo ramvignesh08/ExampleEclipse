1db4a927f971d9ac66f084471ef8d08a945110e9
10721254 <10721254@5CG14493XS.lntinfotech.com>
2023-05-03 11:43:49 +0530

    4th program committed

---
+++ src/Reverse_Number.java
@@ -0,0 +1,16 @@
+
+public class Reverse_Number {
+	public static void main(String[] args) {
+		int number=123456;
+		int b;
+		int sum=0;
+		while(number>0)
+		{
+			b=number%10;
+			sum=(sum*10)+b;
+			number=number/10;
+		}
+		System.out.println(sum);
+	}
+
+}
